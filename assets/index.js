96737f33a3b7e74c372bf2507652d3f0af5c3dea
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-07-09 19:42:47 -0700

    commit changes

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,27 @@
 //Acceptance Criteria
-//Click Start Button - done in CSS/HTML - still need to connect to JS -- startBtn now works with timer; 
-var startBtn = document.getElementById("startBtn") 
+// Referenced study material https://www.youtube.com/watch?v=riDzcEQbX6k
+//Click Start Button 
+
+
+startBtn = document.getElementById("startBtn")
+startPage = document.getElementById("startPage") 
+
+//querySelector worked but not getElementbyClass
+var quizBoxEl = document.querySelector(".quizBox")
+var questionEl = document.getElementById ("question")
+var answerEl = document.getElementById ("userAnswer")
+
+
 // Click start then Timer Begins
 var timerEl = document.getElementById("gameTimer")
 
 startBtn.addEventListener('click', function(){
     var seconds = 60;
-    var countdown = setInterval(function () {
+    countdown = setInterval(function () { // since countdown needs to be re-used when user makes incorrect, putting countdown as global var. removing var
         seconds--;
     if (seconds >= 0) {
         timerEl.textContent = "Seconds remaining: " + seconds; 
-        console.log("seconds:" + seconds);
+        // console.log("seconds:" + seconds);  //keeping until gaveOver function is complete
     } else if (seconds === 0) 
         gameOver(); //game over - need to create function
     
@@ -18,9 +29,53 @@ startBtn.addEventListener('click', function(){
     
 }, 1000);
 })
+
+//Timer runs well
+
+// addEventListener for the quiz
+startBtn.addEventListener('click', startQuiz)
 // Click start then present with a question
 
+
+
+function startQuiz() {
+    startBtn.classList.add('hide');
+    startPage.style.display = 'none';
+    quizBoxEl.classList.remove('hide');
+    score = 0;
+    questionIndex = 0
+    nextQuestion();
+  }
+  
+
+
 // When a question is answered, present another question
+// function taken from youtube but minus the shuffled array
+var questionIndex = 0;
+function nextQuestion() {
+    currentQuestion = quizQuestions[questionIndex];
+    questionIndex++;
+    questionEl.innerText = currentQuestion.question;
+
+
+    currentQuestion.answers.forEach (answer => { 
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerEl.appendChild(button);
+
+    });
+}  
+
+nextQuestion();
+
+function replaceAnswers () {
+    while (answerEl.firstChild) {
+        answerEl.removeChild(answerEl.firstChild);
+    }
+}
+
+
 
 // incorrect answers must result in time penalty of 5 seconds
 
@@ -28,62 +83,59 @@ startBtn.addEventListener('click', function(){
 
 // Game over? Save Initials and Score
 
+//View highscores - button created in HTML
+
 //Creating array of questions
 
-var quizQuestions = [
+
+const quizQuestions = [
     {
         question: "What is the correct syntax for declaring a variable in JavaScript?",
-        answers: {
-        a: 'var myVariable',
-        b: 'createvar myVariable',
-        c: 'const myVariable',
-        d: 'let myVariable'
+        answers: [
+            {text: 'var myVariable', correctAnswer: true },
+            {text: 'createvar myVariable', correctAnswer: false },
+            {text:  'const myVariable', correctAnswer: false },
+            {text:  'let myVariable', correctAnswer: false }
+        ],
     },
-    correctAnswer: 'a'
+    {
+         question: "Which programming language is used for styling web pages?",
+         answers: [
+            {text: 'CSS', correctAnswer: true },
+            {text: 'HTML', correctAnswer: false },
+            {text:  'JavaScript', correctAnswer: false },
+            {text:  'jQuery', correctAnswer: false }
+        ],
     },
+    {
+        question: "Which of the following is NOT a JavaScript framework?",
+        answers: [
+           {text: 'React', correctAnswer: false },
+           {text: 'JAVA', correctAnswer: true },
+           {text:  'Angular', correctAnswer: false },
+           {text:  'Vue', correctAnswer: false }
+       ],
+   },
+   {
+    question: "What is the standard markup language for creating web pages?",
+    answers: [
+       {text: 'Angular', correctAnswer: false },
+       {text: 'Python', correctAnswer: false },
+       {text:  'CSS', correctAnswer: false },
+       {text:  'HTML', correctAnswer: true }
+        ],
+    },
+    {
+    question: "How many values are in a boolean?",
+    answers: [
+       {text: '1 value', correctAnswer: false },
+       {text: 'infinite values', correctAnswer: false },
+       {text:  '2 values', correctAnswer: true},
+       {text:  '0 values', correctAnswer: false }
+   ],
+},
 
-    {
-        question: "Which of the following is not a JavaScript data type?",
-        answers: {
-        a: 'String',
-        b: 'Booleaen',
-        c: 'Numbers',
-        d: 'Arrays'
-    },
-    correctAnswer: 'd'
-    },
 
-    {
-        question: "What does the console.log() function do?",
-        answers: {
-        a: 'Style your website',
-        b: 'Create a new webpage',
-        c: 'Your personal calculator',
-        d: 'Print output to the console'
-    },
-    correctAnswer: 'a'
-    },
-    {
-        question: "Which JavaScript data type has only 2 values?",
-        answers: {
-        a: 'Strings',
-        b: 'Boolean',
-        c: 'Numbers',
-        d: 'Integers'
-    },
-    correctAnswer: 'b'
-    },
-    {
-        question: "What operator in JavaScript is used for strict equality comparisons?",
-        answers: {
-        a: '==',
-        b: '===',
-        c: '!==',
-        d: '!='
-    },
-    correctAnswer: 'b'
-    }
 ]
 
-let question = 0;
-let score = 0;
+
